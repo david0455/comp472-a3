f8b0bbbb2a40877c525d872ec99754b8a5309660
David <davidchen_98@hotmail.com>
2020-12-03 16:52:21 -0500

    implemented predict function of NB

--- src/MNBC.py
+++ src/MNBC.py
@@ -7,19 +7,28 @@
 # https://github.com/jmkovachi/sent-classifier
 
 
+# operator library
+# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 
 
-###### LIKELIHOOD
+ ############### NOTE 1 #################################
 
- #    |     "word1"    |     "word2"    | ... |     "word99"    | 
- # h0 |  P(word1 | h0) |  P(word2 | h0) | ... |  P(word99 | h0) |
- # h1 |  P(word1 | h1) |  P(word2 | h1) | ... |  P(word99 | h1) |
+ #  |     "word1"    |     "word2"    | ... |     "word99"    | 
+ #  |  P(word1 | h0) |  P(word2 | h0) | ... |  P(word99 | h0) |
 
- # likelihood[0,0] = [class, word, probability]
- # e.g. likelihood[0,0] = ["yes", "apple", 0.45]
+ # likelihood = {word: probability}
+ # e.g. likelihood["apple"] <== ["apple", 0.45]
     
 ##########
 
+############## NOTE 2 ###################################
+
+# the tain_set should be formatted as follows:
+# | tweetID | word1 | word2 | word3| ... | class | 
+# | 0000001 |   2   |   3   |  4   | ... |  yes  |
+
+
+import operator as op
 from math import log10
 smoothing = 0.1 
 
@@ -30,18 +39,52 @@ class NB_Classifier:
             "yes": 0, 
             "no": 0
         } 
-        self.likelihood = list()
+        self.likelihood_h0 = {}  # Dictionary of conditional probabilities for h0; see Note 1
+        self.likelihood_h1 = {}  # Dictionary of conditional probabilities for h1; see Note 1
+        self.train_set # dataframe; see Note 2
+        self.vocabulary = list()
+        
 
-    def nb_fit(self, vocabulary):
+    def nb_fit(self, train_set):
+        self.train_set = train_set 
         num_tweet_c = list() # number of instance that are part of that class 
-        tot_num_tweets = vocabulary.length()  # total number of instance/tweets
+        tot_num_tweets = train_set.length()  # total number of instance/tweets
+
+        # TODO: GET all column names (i.e. vocabulary) from dataframe train_set into array
+        self.vocabulary = self.train_set.getColumnName()
 
         for h in self.priors:
-            num_tweet_c = (vocabulary.freq(h))
+            num_tweet_c = (train_set.freq(h))
             self.priors[h] = log10(num_tweet_c/tot_num_tweets)
-            for f in vocabulary:
-                self.likelihood.append([h, f, log10(f.freq()/num_tweet_c)]) # likelihood[0,0] = [class, word, probability]
+
+            #TODO: add smooting
+            for f in self.vocabulary:
+                if h == "yes":
+                    self.likelihood_h0[f] = log10(f.freq()/num_tweet_c) # likelihood[0,0] = [class, word, probability]
+                elif h == "no":
+                    self.likelihood_h1[f] = log10(f.freq()/num_tweet_c) # likelihood[0,0] = [class, word, probability]
 
 
-    def nb(self, vocabulary):
+    def nb(self, test_set):
+        for h in self.priors:
+            score = self.priors[h]
+            for f in test_set:
+                # TODO: Check if tweet has a word in Vocabulary list (training_set)
+                if f.contains(self.vocabulary):
+                    score[h] *= self.likelihood_h0[f]
+
+        result_class = max(score.iteritems(), key=op.itemgetter(1))[0]
+        print(result_class)
+
+    #TODO: Precision
+    def precision(self):
         print("hello")
+    
+    #TODO: Recall
+    def recall(self):
+        print("hello")
+    
+    #TODO: F1-measure
+    def F1(self):
+        print("hello")
+

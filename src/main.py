24271a8ee338534fbb3893577f96f84dfcf7f721
David <davidchen_98@hotmail.com>
2020-12-04 00:13:08 -0500

    implemented main class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+from MNBC import NB_Classifier
+from NB_BOW_OV import getData
 
 def main():
-    print("hello")
+    nb = NB_Classifier()
+    train_set = getData('./data/covid_training.tsv', False)
+    test_set = getData('./data/covid_test_public.tsv', True)
+    nb.fit(train_set)
+    nb.predict2(test_set)
 
 if __name__ == '__main__':
     main()
